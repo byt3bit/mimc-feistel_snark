bf399e1d301b28119e823eabdee831aa0c09b753
byt3bit <NA>
2018-02-13 16:04:47 +0000

    auxiliary header

---
+++ common/utility.hpp
@@ -0,0 +1,15 @@
+#ifndef UTIL_HPP_
+#define UTIL_HPP_
+#include "ntl_header.hpp"
+#include "common.hpp"
+#include <typeinfo>
+#include <sstream>
+
+
+#define BLOCK_MSGE(s) std::clog<<s
+#define FXD_MSG1 "generate constraint (CPU time) "
+#define FXD_MSG2 "generate witness (CPU time) "
+#define FXD_MSG3 "total time (CPU time) "
+
+
+#endif
